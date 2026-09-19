d9a3ab2456d26d5846c2d083a48cc1ff88196260
AndrewLJY <andrewloke9@gmail.com>
2026-09-19 17:13:33 +0800

    Done Week 1 Lab + Checkoff Requirements

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private int currentScore = 0;
+    public GameObject mario;
+    public PlayerMovement playerMovement;
+    public TextMeshProUGUI mainGameScoreText;
+    public TextMeshProUGUI gameOverScoreText;
+    public GameObject MainGameScreen;
+    public GameObject GameOverScreen;
+    public GameObject enemies;
+
+    void Awake()
+    {
+        // Ensure there is only ever one manager
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void AddScore(int amount)
+    {
+        currentScore += amount;
+        UpdateScoreUI();
+    }
+
+    private void UpdateScoreUI()
+    {
+        // Updates both screens simultaneously 
+        if (mainGameScoreText != null) mainGameScoreText.text = "Score: " + currentScore;
+        if (gameOverScoreText != null) gameOverScoreText.text = "Score: " + currentScore;
+    }
+
+    public void RestartButtonCallback(int input)
+    {
+        Debug.Log("Restart!");
+        // reset everything
+        ResetGame();
+        // resume time
+        Time.timeScale = 1.0f;
+    }
+
+    private void ResetGame()
+    {
+        // reset position
+        mario.GetComponent<Transform>().position = new Vector3(-0.947f, -0.292f, 0.0f);
+        // reset sprite direction
+        playerMovement.faceRightState = true;
+        mario.GetComponent<SpriteRenderer>().flipX = false;
+        // reset Goomba
+        foreach (Transform eachChild in enemies.transform)
+        {
+            eachChild.transform.localPosition = eachChild.GetComponent<EnemyMovement>().startPosition;
+        }
+        // reset score
+        currentScore = 0;
+        UpdateScoreUI();
+        MainGameScreen.SetActive(true);
+        GameOverScreen.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private SpriteRenderer marioSprite;
+    public bool faceRightState = true;
+    public float speed = 10;
+    public float maxSpeed = 20;
+    private Rigidbody2D marioBody;
+    public float upSpeed = 10;
+    private bool onGroundState = true;
+    public TextMeshProUGUI scoreText;
+    public GameObject enemies;
+    public GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        marioSprite = GetComponent<SpriteRenderer>();
+
+        // Set to be 30 FPS
+        Application.targetFrameRate = 30;
+        marioBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a") && faceRightState)
+        {
+            faceRightState = false;
+            marioSprite.flipX = true;
+        }
+
+        if (Input.GetKeyDown("d") && !faceRightState)
+        {
+            faceRightState = true;
+            marioSprite.flipX = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
+    }
+
+    // FixedUpdate is called 50 times a second
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        if (Mathf.Abs(moveHorizontal) > 0)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            // check if it doesn't go beyond maxSpeed
+            if (marioBody.linearVelocity.magnitude < maxSpeed)
+                marioBody.AddForce(movement * speed);
+        }
+
+        // stop
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+        {
+            // stop
+            marioBody.linearVelocity = Vector2.zero;
+        }
+
+        if (Input.GetKeyDown("space") && onGroundState)
+        {
+            marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collided with goomba!");
+            Time.timeScale = 0.0f;
+            gameManager.MainGameScreen.SetActive(false);
+            gameManager.GameOverScreen.SetActive(true);
+        }
+    }
+
+    
+}
